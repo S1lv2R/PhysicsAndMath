e2368123bf9393628b7417edf062e4b02da5715b
Thanh Duy <sologame37@gmail.com>
2023-12-15 22:51:06 +0700

    Create data_processing.py

---
+++ VM2C_Final/data_processing.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+shift_time = np.zeros((29, 3), dtype=int)
+
+def convert_to_start_time(_time) -> int:
+    if 6 <= _time < 14:
+        return 6
+    elif 14 <= _time < 22:
+        return 14
+    else:
+        return 22
+
+def convert_to_end_time(_time) -> int:
+    if 6 < _time <= 14:
+        return 14
+    elif 14 < _time <= 22:
+        return 22
+    else:
+        return 30
+
+with open(f"data.txt", "r") as f:
+    f.readline()  # Comment line
+    _time = [line.strip().split() for line in f.readlines()]
+
+    shift_start_time = [6, 14, 22]
+
+    for i in range(len(_time)):
+        start_day = int(_time[i][0][8:10])
+        start_hour = int(_time[i][1][0:2])
+
+        end_day = int(_time[i][2][8:10])
+        end_hour = int(_time[i][3][0:2])
+
+        if (start_hour >= 0 and start_hour < shift_start_time[0]):
+            shift_time[start_day - 1, 2] = 1
+        
+        new_start_hour = convert_to_start_time(start_hour)
+        new_end_hour = convert_to_end_time(end_hour)
+        active_shifts = (new_end_hour - new_start_hour) // 8
+        
+        shifts = [1] * active_shifts
+        if (new_start_hour == shift_start_time[0]):
+            shift_time[start_day, :active_shifts] = shifts
+        elif (new_start_hour == shift_start_time[1]):
+            shift_time[start_day, 1:(active_shifts + 1)] = shifts
+            if active_shifts > 2:
+                shift_time[start_day + 1, 0] = 1
+        elif (new_start_hour == shift_start_time[2]):
+            shift_time[start_day, 2] = 1
+            if active_shifts > 1:
+                shift_time[start_day + 1, :active_shifts] = shifts
+
+print(shift_time)
